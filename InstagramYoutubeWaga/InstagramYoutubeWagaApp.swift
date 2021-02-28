2d0b65e13fe34628b61b4b6c27469fa9331bc70a
tanvirgeekios <tanvir.alam@iosventure.com>
2021-02-28 10:53:29 +0600

    Initial Commit

---
+++ InstagramYoutubeWaga/InstagramYoutubeWagaApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramYoutubeWagaApp.swift
+//  InstagramYoutubeWaga
+//
+//  Created by MD Tanvir Alam on 28/2/21.
+//
+
+import SwiftUI
+
+@main
+struct InstagramYoutubeWagaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
